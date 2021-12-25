adfb2d8487f541e324966d35cc0004605fa7875e
prakharjain1210 <77112931+prakharjain1210@users.noreply.github.com>
2021-12-25 11:50:03 +0530

    Add files via upload

---
+++ p98.py
@@ -0,0 +1,10 @@
+def swapFileData():
+    with open("sample1.txt","r") as a:
+        data_a=a.read()
+    with open("sample2.txt","r") as b:
+        data_b=b.read()
+    with open("sample1.txt","w") as a:
+        a.write(data_b)
+    with open("sample2.txt","w") as b:
+        b.write(data_a)
+swapFileData()    
